a3b15fba4a12ebf2aa989780e2e4f61e21622f2d
arkaan27 <arkaankanunga@gmail.com>
2022-02-14 15:09:54 +0000

    Updated train_model to include screenshots

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -3,14 +3,16 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+import os
+import joblib
 import pandas as pd
-import pickle
-
 from ml.data import process_data
-from ml.model import train_model, inference, compute_model_metrics, evaluate_model_on_column_slices
+from ml.model import compute_model_metrics, inference, train_model
+from data_slice import data_slicing_categorical
 
 # Add code to load in the data.
-data = pd.read_csv('../data/cleaned_census.csv')
+dirname = os.path.dirname(__file__)
+data = pd.read_csv(os.path.join(dirname, "../data/cleaned_census.csv"))
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -30,25 +32,23 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
-X_test, y_test, _, _ = process_data(
-    test, categorical_features=cat_features, label="salary", training=False,
-    encoder=encoder, lb=lb
+X_test, y_test, encoder, lb = process_data(
+    test,
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb,
 )
+# Train and save model and encoder
+trained_model = train_model(X_train, y_train)
+joblib.dump(trained_model, os.path.join(dirname, "../model/model.joblib"))
+joblib.dump(encoder, os.path.join(dirname, "../model/encoder.joblib"))
 
-# Train and save a model.
-model = train_model(X_train, y_train)
-with open('../model/model.pkl', 'w+b') as f:
-    pickle.dump(model, f)
-with open('../model/encoder.pkl', 'w+b') as f:
-    pickle.dump(encoder, f)
+# Data slicing function on certain column (using education as example)
+data_slicing_categorical(test, cat_features, trained_model, encoder, lb, "education")
 
-
-# Evaulate the model
-y_pred = inference(model, X_test)
-overall_metric = compute_model_metrics(y_test, y_pred)
-
-slice_metrics = evaluate_model_on_column_slices(test, 'education', y_test, y_pred)
-
-metric_df = pd.DataFrame(slice_metrics, columns=['education_slice', 'precision', 'recall', 'f1'])
-metric_df.to_csv('../model/slice_output.txt', index=False)
-
+# Show the overall performance for writing model card
+preds = inference(trained_model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+print(f"Overall Performance: precision:{precision}, recall:{recall}, fbeta:{fbeta}")
