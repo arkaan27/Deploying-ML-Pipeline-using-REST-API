5ea6501e6a6b124697b89f339ef9c497fb0476cc
arkaan27 <arkaankanunga@gmail.com>
2022-02-14 13:08:24 +0000

    Added api-script

---
+++ api-script.py
@@ -0,0 +1,15 @@
+import requests
+
+host = 'https://ml-model-fast-api.herokuapp.com/'
+predict_uri = f"{host}/predict"
+
+item = {'age': 42, 'workclass': 'Private', 'fnlgt': 159449, 'education': 'Bachelors', 'education-num': 13,
+        'marital-status': 'Married-civ-spouse', 'occupation': 'Exec-managerial', 'relationship': 'Husband',
+        'race': 'White', 'sex': 'Male', 'capital-gain': 5178, 'capital-loss': 0, 'hours-per-week': 40,
+        'native-country': 'United-States'}
+
+r = requests.post(predict_uri, json=item)
+
+print('Request body: ', item)
+print('Status code: ', r.status_code)
+print('Response: ', r.json())
