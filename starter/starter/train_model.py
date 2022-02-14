7c4245db103895d0d81789e2626aec5d0edfc701
arkaan27 <arkaankanunga@gmail.com>
2022-02-14 12:23:24 +0000

    Finished Train model script

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -7,10 +7,11 @@ import pandas as pd
 import pickle
 
 from ml.data import process_data
-from ml.model import train_model,compute_model_metrics,inference
-# Add code to load in the data.
+from ml.model import train_model, inference, compute_model_metrics, evaluate_model_on_column_slices
 
-data = pd.read_csv()
+# Add code to load in the data.
+data = pd.read_csv('../data/cleaned_census.csv')
+
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
@@ -29,5 +30,25 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=False,
+    encoder=encoder, lb=lb
+)
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+with open('../model/model.pkl', 'w+b') as f:
+    pickle.dump(model, f)
+with open('../model/encoder.pkl', 'w+b') as f:
+    pickle.dump(encoder, f)
+
+
+# Evaulate the model
+y_pred = inference(model, X_test)
+overall_metric = compute_model_metrics(y_test, y_pred)
+
+slice_metrics = evaluate_model_on_column_slices(test, 'education', y_test, y_pred)
+
+metric_df = pd.DataFrame(slice_metrics, columns=['education_slice', 'precision', 'recall', 'f1'])
+metric_df.to_csv('../model/slice_output.txt', index=False)
+
